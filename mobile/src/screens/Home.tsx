33a4f64934f892b944299fa33c5a9a8473a6d926
Raphael Faria <raphaelsimoes@gmail.com>
2023-01-20 01:40:11 -0300

    aula 3

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -13,6 +14,9 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+
+    const { navigate } = useNavigation();
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -40,6 +44,7 @@ export function Home() {
                         datesFromYearStart.map(date => (
                             <HabitDay
                                 key={date.toISOString()}
+                                onPress={() => navigate('habit', { date: date.toISOString() })}
                             />
                         ))
                     }
@@ -47,20 +52,20 @@ export function Home() {
                     {
                         amountOfDaysToFill > 0 && Array
                             .from({ length: amountOfDaysToFill })
-                            .map((_, i) => (
-                                <View
-                                    className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
-                                    style={{
-                                        width: DAY_SIZE, height: DAY_SIZE
-                                    }}
-                                />
-                            ))
+                            .map((_, i) => {
+                                return (
+                                    <View
+                                        key={i}
+                                        className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                                        style={{
+                                            width: DAY_SIZE, height: DAY_SIZE
+                                        }}
+                                    />
+                                )
+                            })
                     }
                 </View>
             </ScrollView>
-
-
-
         </View>
     )
 }
